6fdd1268ed3fba72e1aefa784100778ff269c2cd
Drew Holzworth <drew@holzworth.info>
2022-10-24 14:11:20 +1100

    WIP commit

--- scripts/cfa-analysis.r
+++ scripts/cfa-analysis.r
@@ -3,13 +3,15 @@ library('raster')
 
 data_directory = '.data'
 
+#' Read an LPJ-Guess output file and return file data as a dataframe.
+#' @param filename: Path to the output file, may be relative or absolute.
 read_lpj_guess_output_file <- function(filename) {
   return(read.table(filename, header = T))
 }
 
 output_data_to_raster <- function(data, year, column) {
   # Filter data to the specified year and PFT.
-  filtered <- data[data$Year == year,c('Lon', 'Lat', column)]  
+  filtered <- data[data$Year == year,c('Lon', 'Lat', column)]
 
   # Generate a raster for this data.
   return(rasterFromXYZ(filtered))
@@ -30,8 +32,9 @@ get_full_filename <- function(filename) {
   return(paste0(data_directory, '/', filename))
 }
 
-create_animation <- function(data, out_file, column, title, ymax = -1,
-  width = 480, height = 480, frame) {
+#' @param rescale: Set to true to make y axis symmetrical around 0.
+create_animation <- function(data, out_file, column, title, ymin = -1,
+  ymax = -1, width = 480, height = 480, colours = NULL, rescale = F) {
   # Setting up a few plot-related settings.
   if (ymax == -1) {
     # Limit y axis (z-axis, really) to whichever is smaller:
@@ -43,7 +46,21 @@ create_animation <- function(data, out_file, column, title, ymax = -1,
     ymax <- min(max(data[[column]]), 
                 mean(data[[column]]) + 3 * sd(data[[column]]))
   }
-  ylim <- c(min(data[[column]]), ymax)
+  if (ymin == -1) {
+    # See above comment RE ymax for details.
+    ymin <- max(min(data[[column]]),
+                mean(data[[column]]) - 3 * sd(data[[column]]))
+  }
+
+  if (rescale && ymin < 0 && ymax > 0) {
+    ymax <- max(abs(ymin), ymax)
+    ymin <- -ymax
+  }
+  if (is.null(colours)) {
+    colours = rev(terrain.colors(255))
+  }
+
+  ylim <- c(ymin, ymax)
   title <- paste0(title, ':')
 
   # Generate the animation.
@@ -53,7 +70,7 @@ create_animation <- function(data, out_file, column, title, ymax = -1,
       rs <- output_data_to_raster(data, year, column)
 
       # Calling plot() will add the frame to the animation.
-      plot(rs, main = paste(title, year), zlim = ylim, interpolate = T)
+      plot(rs, main = paste(title, year), zlim = ylim, interpolate = F, col = colours)
     }
   # These options could be user-configurable too.
   }, interval = 0.2, movie.name = out_file, ani.width = width,
@@ -65,6 +82,56 @@ create_animation_from_file <- function(in_file, out_file, column, title) {
   create_animation(data, out_file, column, title)
 }
 
+# Animation settings for the delta graphs.
+anim <- function(data, column, filename, title, width, height) {
+  create_animation(data, filename, column, title,
+                   colours = hcl.colors(255, 'red-green'), rescale = T,
+                   width = width, height = height)
+}
+
+animate_delta <- function(data0, data1, filename, title, width, height, column) {
+  data_columns <- c("BNE", "BINE", "BNS", "TeNE", "TeBS", "IBS", "TeBE", "TrBE",
+                    "TrIBE", "TrBR", "C3G", "C4G", "Total")
+  deltas <- data.frame(data0)
+  for (col in data_columns) {
+    deltas[[column]] <- data0[[column]] - data1[[column]]
+  }
+  anim(deltas, column, filename, title, width, height)
+}
+
+#' Create animations for deltas between the different australian runs.
+#' This will take a few minutes. Reducing width and height will make it faster.
+#' @param width: Output animation width (px)
+#' @param height: Output animation height(px)
+create_delta_animations <- function(width = 1280, height = 1280) {
+  
+  base_file <- get_full_filename('australia.out')
+  hico2_file <- get_full_filename('australia_elevated_co2.out')
+  hico2_hitemp_file <- get_full_filename('australia_hot.out')
+
+  base_data <- read_lpj_guess_output_file(base_file)
+  hico2_data <- read_lpj_guess_output_file(hico2_file)
+  hico2_hitemp_data <- read_lpj_guess_output_file(hico2_hitemp_file)
+
+  # Generate animations of raw data.
+  create_animation(base_data, 'au-baseline.gif', 'Total', 'LAI', width = width,
+                   height = height)
+  create_animation(hico2_data, 'au-highco2.gif', 'Total', 'LAI at +150ppm CO2',
+                   width = width, height = height)
+  create_animation(hico2_hitemp_data, 'au-highco2-hightemp', 'Total',
+                   'LAI at +2°C, +150ppm CO2', width = width, height = height)
+
+  # Generate animations of LAI deltas.
+  animate_delta(hico2_data, base_data, 'highco2_vs_baseline_delta.gif',
+       'LAI 𝚫 at +150ppm CO2', width, height)
+
+  animate_delta(hico2_hitemp_data, base_data, 'highco2_hightemp_vs_baseline_delta.gif',
+       'LAI 𝚫 at +2°C, +150ppm CO2', width, height)
+
+  animate_delta(hico2_hitemp_data, hico2_data, 'highco2_hightemp_vs_highco2_delta.gif',
+       'LAI 𝚫 at +2°C, +150ppm CO2 vs +150ppm CO2', width, height)
+}
+
 au_file <- get_full_filename('australia.out')
 au_elev_co2 <- get_full_filename('australia_elevated_co2.out')
 au_elev_temp_co2 <- get_full_filename('australia_hot.out')
@@ -74,4 +141,7 @@ historical_files <- c(
   get_full_filename('historical_CSIRO-BOM-ACCESS1-3_UNSW-WRF360J.out'),
   get_full_filename('historical_CSIRO-BOM-ACCESS1-3_UNSW-WRF360K.out')
 )
+
+data_columns <- c("BNE", "BINE", "BNS", "TeNE", "TeBS", "IBS", "TeBE", "TrBE",
+                  "TrIBE", "TrBR", "C3G", "C4G", "Total")
 
